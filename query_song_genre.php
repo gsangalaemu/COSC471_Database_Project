1d7e72a097a9caa9360c1a2eae728c529e4666e8
gsangalaemu <gsangala@emich.edu>
2025-11-16 05:01:59 -0500

    Added query songs by genre php script and verified that it is working

--- query_song_genre.php
+++ query_song_genre.php
@@ -10,7 +10,7 @@ $password = "";
 $database = "song_sleuth";
 
 // The artist the user input
-$artist = $_POST['querySongArtist'];
+$genre = $_POST['querySongGenre'];
 
 // Create connection to database
 $conn = new mysqli($host, $user, $password, $database);
@@ -22,13 +22,9 @@ if ($conn->connect_error) {
 
 // We want to print the artist name, the album name, the release date, and the genre.
 // We have to join quite a few tables to be able to print all the data that we want
-// The LEFT JOINs are needed so that we can properly search both the primary and
-// guest/featured/performing artists for a song.
-// GROUP_CONCAT is needed so that songs that appear on multiple albums will list
-// all albums the song appears on, as well as songs that have multiple
-// featured/guest/performing artists all appear in one cell.
+// GROUP_CONCAT is used so that albums with multiple genres will display all genres
 // The COLLATE is needed to ignore case sensitivity issues
-$sql = "SELECT A.Artist_name, S.Song_name, S.Year, G.Genre_name,
+$sql = "SELECT A.Artist_name, S.Song_name, S.Year, G.Genre_name, S.Song_id,
                GROUP_CONCAT(B.Album_name) AS Album_name,
                GROUP_CONCAT(FA.Artist_name) AS Performing_artist
         FROM ARTISTS AS A
@@ -38,39 +34,35 @@ $sql = "SELECT A.Artist_name, S.Song_name, S.Year, G.Genre_name,
         JOIN GENRES AS G ON G.Genre_id = S.Genre
         JOIN SONG_ALBUMS AS SA ON SA.Song_id = S.Song_id
         JOIN ALBUMS AS B ON B.Album_id = SA.Album_id
-        WHERE A.Artist_name COLLATE utf8_unicode_ci = ?
-              OR PA.Featured_artist IN (SELECT Artist_id
-                                        FROM ARTISTS
-                                        WHERE Artist_name COLLATE utf8_unicode_ci = ?)
+        WHERE G.Genre_name COLLATE utf8_unicode_ci = ?
         GROUP BY S.Song_id";
-
 
 // Prepare and bind the the variable
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("ss", $artist, $artist);
+$stmt->bind_param("s", $genre);
 $stmt->execute();
 $result = $stmt->get_result();
 
 if ($result->num_rows> 0) {
     // Print a table of results
-    echo "<h1>" . "Songs " . $artist . " performs on" . "</h1>";
+    echo "<h1>" . "Songs matching " . $genre . "</h1>";
     echo "<table>
         <tr>
+            <th>Genre</th>
             <th>Album  Artist</th>
             <th>Featured Artist(s)</th>
-            <th>Song</th>            
-            <th>Year</th>
+            <th>Song</th>
             <th>Appears On</th>
-            <th>Genre</th>
+            <th>Year</th>            
         </tr>";
     while ($row = $result->fetch_assoc()) {
         echo "<tr>" .
+        "<td>" . $row["Genre_name"] . "</td>" .
         "<td>" . $row["Artist_name"] . "</td>" .
         "<td>" . $row["Performing_artist"] . "</td>" .
-        "<td>" . $row["Song_name"] . "</td>" .        
+        "<td>" . $row["Song_name"] . "</td>" .
         "<td>" . $row["Year"] . "</td>" .
         "<td>" . $row["Album_name"] . "</td>" .
-        "<td>" . $row["Genre_name"] . "</td>" .
         "</tr>";
     }
     echo "</table>";
