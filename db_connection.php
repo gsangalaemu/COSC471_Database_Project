7def37e2577fc43c91541bd3073211405987870a
Andrew Hainsworth <ahainsw1@emich.edu>
2025-11-24 23:15:14 -0500

    Commented db setup

--- db_connection.php
+++ db_connection.php
@@ -4,8 +4,10 @@ $user = "root";
 $pass = ""; 
 $db   = "song_sleuth";
 
+// Create new connection 
 $conn = new mysqli($host, $user, $pass, $db);
 
+// Kill connection if error is thrown 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
